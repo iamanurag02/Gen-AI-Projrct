48ba2640a3eefb760bbccc99b500303be33e9dd1
Anurag Pathak <anurag.pathak0207@gmail.com>
2026-07-14 04:26:58 +0000

    Fix MongoDB connection for Mongoose 9

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,17 +1,32 @@
 const mongoose = require("mongoose")
 
+let isConnected = false;
 
-
-async function connectToDB() {
-
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log("Connected to Database")
+async function connectToMongoDB() {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
     }
-    catch (err) {
-        console.log(err)
-    }
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
 }
 
-module.exports = connectToDB
+// async function connectToDB() {
+
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI)
+
+//         console.log("Connected to Database")
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
+
+module.exports = connectToMongoDB
